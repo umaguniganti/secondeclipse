55cbaaaa9f429d2016a4d22ff504a1e45bd626e6
umaguniganti <58178913+umaguniganti@users.noreply.github.com>
2023-12-14 15:33:16 +0530

    version1.0

---
+++ src/test/java/gitproject/demo.java
@@ -0,0 +1,12 @@
+package gitproject;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is the first git project");
+
+	}
+
+}
