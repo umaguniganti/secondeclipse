2946f2c0e5762c9e34878644e70e4d25db0b65f5
mahesh <umamaheswar24@gmail.com>
2024-01-16 10:51:25 +0530

    first code check in by team member

--- src/test/java/gitproject/demo.java
+++ src/test/java/gitproject/demo.java
@@ -5,32 +5,10 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is the first git project");
-		System.out.println("Team member01 - This is the second git project");
+		//First Code by team member
 		
-		// 12/16 1:21 pm changes
+		System.out.println("First code check in by team member");
 		
-		System.out.println("Second code check in from team member01");
-
-		System.out.println("Third code check in from team member01");
-
-		
-		// 12/16 2:16 pm changes
-		
-				System.out.println("first code check in from team lead");
-				
-				// 12/16 2:16 pm changes
-				
-				System.out.println("second code check in from team lead");
-				
-				
-				//1/14/2024
-				
-				System.out.println("Boghi day check in from team lead");
-				
-	//1/15/2024
-				
-				System.out.println("Sankranthi day check in from team lead");
 				
 				
 
