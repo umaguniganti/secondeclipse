982146e6d561a79303fa8b6ff947dfd64f26cedc
umaguniganti <umamaheswar24@gmail.com>
2024-05-27 15:07:42 +0530

    saved details on 5/27

--- src/test/java/gitproject/demo.java
+++ src/test/java/gitproject/demo.java
@@ -27,7 +27,7 @@ public class demo {
 				
 				//added new feature 5/27/2024
 
-				System.out.println("code update done by team member");
+				System.out.println("code update done by team member 5/27/2024");
 	}
 
 }
